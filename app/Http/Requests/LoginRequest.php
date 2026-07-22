9a00468c3aa5967b4e397beaba6c2dcfed30b254
thanhdt05 <thanhdt.workmail@gmail.com>
2026-07-22 13:51:42 +0700

    refactor: restrict login authorization and enhance validation rules with custom error messages

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -12,19 +12,39 @@ class LoginRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return false;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
-            'email' => ['required', 'string', 'email'],
-            'password' => ['required', 'string'],
+            "email" => "required|email|string|max:255",
+            "password" => "required|string|min:8|max:255",
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            "email.required" => "Email is required",
+            "email.max" => "Email must be at most 255 characters long",
+            "email.email" => "Email is invalid",
+            "password.required" => "Password is required",
+            "password.min" => "Password must be at least 8 characters long",
+            "password.max" => "Password must be at most 255 characters long",
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            "email" => "Email",
+            "password" => "Password",
         ];
     }
 }
